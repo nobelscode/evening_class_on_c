63b008999de43d9584c973f668b71b0e4f510e4d
Nobel S <nobelsng@gmail.com>
2021-02-16 15:37:58 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int chimberly[3] = {12,10,15};
+  int sum = chimberly[0] + chimberly[1];
+  char abdulmalik[4] = {'r','i','c','e'};
+
+  printf("the sum of two numbers is %d\n",sum);
+  printf("the first letter  is %c",abdulmalik[1]);
+  return 0;
+}
